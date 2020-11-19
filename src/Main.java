6482081a5d8ccb3fdcfffa04fe964618676c2d22
Maxim Mikheyev <mma210977@gmail.com>
2020-11-19 14:10:20 +0700

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        boolean registered = true;
+        int Tickets = 24510;
+        byte Rubles = 20;
+        int Miles= Tickets / Rubles;
+        System.out.println (Miles);
+    }
+}
