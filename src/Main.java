56e2d4ecb45d8aa9034a01475bbeb5eff069e10c
Maxim Mikheyev <mma210977@gmail.com>
2020-11-20 13:57:35 +0700

    Main.java modified

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
-    public static void main(String[] args)
-    {
-        boolean registered = true;
-        int Tickets = 24510;
-        byte Rubles = 20;
-        int Miles= Tickets / Rubles;
-        System.out.println (Miles);
+    public static void main(String[] args) {
+        int tickets = 24510;
+        byte rubles = 20;
+        int miles = tickets / rubles;
+        System.out.println(miles);
     }
 }
